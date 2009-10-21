b51e01ab16bfebe4158bca560471bbc77c5f7a5b
YAMAGUCHI Kiyohiro <kiyoya@gmail.com>
2009-10-22 00:24:22 +0900

    Translate jp to ja

--- sociarium/module/language_en.cpp
+++ sociarium/module/language_en.cpp
@@ -1,4 +1,4 @@
-﻿// s.o.c.i.a.r.i.u.m: module/language_jp.cpp
+﻿// s.o.c.i.a.r.i.u.m: module/language_en.cpp
 // HASHIMOTO, Yasuhiro (E-mail: hy @ sys.t.u-tokyo.ac.jp)
 
 /* Copyright (c) 2005-2009, HASHIMOTO, Yasuhiro, All rights reserved.
